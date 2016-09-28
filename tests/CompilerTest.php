01781632cb40e43fd47e68e0dbf823057dd40277
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-09-28 10:06:38 +0300

    init composer

---
+++ tests/CompilerTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright (c) 2016.
+ *
+ * @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ */
+
+class CompilerTest extends PHPUnit_Framework_TestCase
+{
+    public function testCombiner()
+    {
+
+    }
+}
